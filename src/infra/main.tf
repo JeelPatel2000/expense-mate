36b8c3bfa815558f3e92af295450e36fa9bc05c5
Jeel Patel <jeel.patel1111@gmail.com>
2023-07-08 00:40:24 +0800

    add security groups

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -7,11 +7,45 @@ provider "random" {}
 resource "random_pet" "name" {}
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0ed828ae690ef8b35"
-  instance_type = "t2.micro"
-	user_data = file("./init-script.sh")
+  ami             = "ami-0ed828ae690ef8b35"
+  instance_type   = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow-http.id]
 
   tags = {
     Name = random_pet.name.id
+  }
+}
+resource "aws_security_group" "allow-http" {
+  name = "${random_pet.name.id}-sg"
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- src/infra/terraform.tf
+++ src/infra/terraform.tf
@@ -1,18 +1,18 @@
 terraform {
-    backend "remote" {
-		organization = "expense-mate"
-		workspaces {
-			name = "expense-mate"
-		}
-	}
-    required_providers {
-        random = {
-            source = "hashicorp/random"
-        }
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.16"
-        }
+  backend "remote" {
+    organization = "expense-mate"
+    workspaces {
+      name = "expense-mate"
     }
-    required_version = ">= 1.2.0"
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
 }
