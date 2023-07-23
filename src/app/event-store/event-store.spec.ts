e9284f90103e30e7c3e5aa2c68d8aa892b0f7774
Jeel Patel <jeel.patel1111@gmail.com>
2023-07-24 00:19:06 +0800

    setup tdd

--- src/app/event-store/event-store.spec.ts
+++ src/app/event-store/event-store.spec.ts
@@ -1,6 +1,22 @@
-import { EventStore } from './event-store'
+import { EventStore, inMemoryEventStore } from './event-store'
+import { EventEnvelope, PersistedEventEnvelope, StreamType } from './types';
 
 
-const eventStoreTest = (eventStore: EventStore) => (): void => {
+const eventStoreTest = (eventStore: EventStore, teardown?: () => Promise<void>) => (): void => {
+  afterAll(async () => {
+    if(teardown)
+      await teardown();
+  })
 
+  it('passing test', () => {
+    expect(true).toBe(true)
+  })
+  
 }
+
+describe('Event store tests', () => {
+
+  describe('In memory eventstore test', eventStoreTest(inMemoryEventStore()))
+
+  
+})
--- src/app/event-store/event-store.ts
+++ src/app/event-store/event-store.ts
@@ -12,3 +12,26 @@ export interface EventStore {
   appendStreams: (streams: StreamEvents[]) => Promise<void>
   readStream: <T>(streamId: string, streamType: StreamType) => Promise<Array<PersistedEventEnvelope<T>>>
 }
+
+export const inMemoryEventStore = (): EventStore => {
+
+  const append = async (streamType: StreamType, streamId: string, expectedVersion: number, events: EventEnvelope[]) => {
+    return
+  }
+
+  const appendStreams = async (streams: StreamEvents[]) => {
+    return
+  }
+
+  const readStream = async (streamId: string, streamType: StreamType): Promise<Array<PersistedEventEnvelope<any>>>  => {
+    const result: Array<PersistedEventEnvelope<any>> = []
+    return result
+  }
+
+
+  return {
+    append,
+    appendStreams,
+    readStream
+  }
+}
