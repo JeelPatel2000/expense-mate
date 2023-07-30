4a8ad721be5cfbcdf139978b18e0bb2d9879e05c
Jeel Patel <34994081+JeelPatel2000@users.noreply.github.com>
2023-07-31 00:45:29 +0800

    add more test

--- src/app/event-store/event-store.spec.ts
+++ src/app/event-store/event-store.spec.ts
@@ -1,6 +1,6 @@
 import { getTestDBPool } from '../utils/db';
-import { EventStore, inMemoryEventStore, postgresEventStore } from './event-store'
-import { StreamType } from './types';
+import { EventStore, StreamEvents, inMemoryEventStore, postgresEventStore } from './event-store'
+import { EventEnvelope, StreamType } from './types';
 
 
 const eventStoreTest = (eventStore: EventStore, timestamp: Date, teardown?: () => Promise<void>) => (): void => {
@@ -9,9 +9,18 @@ const eventStoreTest = (eventStore: EventStore, timestamp: Date, teardown?: () =
       await teardown();
   })
 
+  const givenEvent = (streamType: StreamType = StreamType.Expense, streamId: string = `stream-01`, expectedVersion: number = 0, events: EventEnvelope<any>[] = []): StreamEvents => {
+    return {
+      events,
+      expectedVersion,
+      streamId, 
+      streamType
+    }
+  }
+
   it(`given no events > when reading stream > then return empty array`, async () => {
     const events = await eventStore.readStream(`stream1`, StreamType.User)
-    expect(events).toEqual([])
+    expect(events).toStrictEqual([])
   })
 
   it(`given events > when append event > then appends event`, async () => {
@@ -21,11 +30,27 @@ const eventStoreTest = (eventStore: EventStore, timestamp: Date, teardown?: () =
     
     
     expect(await eventStore.readStream(`stream1`, StreamType.Expense)).
-      toEqual([{ eventType: 'Created', metadata: {}, payload: { eventType: `Created` }, position: `1`, streamId: `stream1`, streamType: StreamType.Expense, timestamp, version: 1 }])
+      toStrictEqual([{ eventType: 'Created', metadata: {}, payload: { eventType: `Created` }, position: `1`, streamId: `stream1`, streamType: StreamType.Expense, timestamp, version: 1 }])
     expect(await eventStore.readStream(`stream2`, StreamType.Expense)).
-      toEqual([
+      toStrictEqual([
         { eventType: 'Created', metadata: {}, payload: { eventType: `Created` }, position: `2`, streamId: `stream2`, streamType: StreamType.Expense, timestamp, version: 1 },
         { eventType: 'Updated', metadata: {}, payload: { eventType: `Updated` }, position: `3`, streamId: `stream2`, streamType: StreamType.Expense, timestamp, version: 2 },
+      ])
+  })
+
+  it(`given multiple event streams > when appenStreams > then appends streams`, async () => {
+    const streams: StreamEvents[] = [
+      givenEvent(StreamType.Expense, `expense-003`, 0, [{ eventType: `Created`, metadata: {}, payload: { eventType: `Created` }}]),
+      givenEvent(StreamType.Expense, `expense-004`, 0, [{ eventType: `Updated`, metadata: {}, payload: { eventType: `Updated` }}]),
+    ]
+    await eventStore.appendStreams(streams)
+    expect(await eventStore.readStream(`expense-003`, StreamType.Expense)).
+      toStrictEqual([
+        { eventType: 'Created', metadata: {}, payload: { eventType: `Created` }, position: `4`, streamId: `expense-003`, streamType: StreamType.Expense, timestamp, version: 1 }
+      ])
+    expect(await eventStore.readStream(`expense-004`, StreamType.Expense)).
+      toStrictEqual([
+        { eventType: 'Updated', metadata: {}, payload: { eventType: `Updated` }, position: `5`, streamId: `expense-004`, streamType: StreamType.Expense, timestamp, version: 1 }
       ])
   })
 }
