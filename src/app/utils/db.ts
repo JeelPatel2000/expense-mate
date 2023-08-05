e5a7728fc17a2d7415511d30e06f0f321643b720
Jeel Patel <34994081+JeelPatel2000@users.noreply.github.com>
2023-08-05 15:08:49 +0800

    use environment variables

--- src/app/utils/db.ts
+++ src/app/utils/db.ts
@@ -2,11 +2,11 @@ import { Pool } from "pg"
 
 export const getTestDBPool = () => {
     return new Pool({
-        host: `localhost`,
-        port: 5432,
-        user: `postgres`,
-        password: `postgres`,
-        database: `expense_mate`
+        host: process.env.POSTGRES_HOST ?? `localhost`,
+        port: Number(process.env.POSTGRES_PORT ?? 5432),
+        user: process.env.POSTGRES_USER ?? `postgres`,
+        password: process.env.POSTGRES_PASSWORD ??`postgres`,
+        database: process.env.POSTGRES_DB ?? `expense_mate`
     })
 }
 
